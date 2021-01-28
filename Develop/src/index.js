86dbd9ab04e7201d3751c1561ac6ffcb13b970be
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-28 16:41:10 -0600

    Added employee Constructor function.

---
+++ Develop/src/index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./Employee");
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+const pageTemplate = require("./page-template");
+
+
