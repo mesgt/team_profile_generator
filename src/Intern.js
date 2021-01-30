054b820aec7dbe615a50594c09d5d98d2cb6426e
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-29 19:40:42 -0600

    Intern subclass has passed all tests.

--- src/Intern.js
+++ src/Intern.js
@@ -1,3 +1,27 @@
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email)
+        this.school = school;
+    }
+
+getRole() {
+    return "Intern";
+}
+
+getSchool() {
+    return this.school;
+}
+}
+
+module.exports = Intern;
+
+
+
+
+
+
 // function Intern(name, employeeID, email, school) {
 //     if (typeof name !== "string" || !name.trim().length) {
 //         throw new Error("Expected parameter 'name' to be a non-empty string");
