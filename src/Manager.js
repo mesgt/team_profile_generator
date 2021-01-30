013917febf33827745cc59f46adfdbe126249ea8
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-29 19:47:28 -0600

    Manager subclass passed all tests.

--- src/Manager.js
+++ src/Manager.js
@@ -1,3 +1,23 @@
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber){
+        super(name, id, email)
+        this.officeNumber = officeNumber;
+    }
+    
+getRole() {
+    return "Manager";
+}
+
+getOfficeNumber() {
+    return this.officeNumber;
+}
+}
+
+module.exports = Manager;
+
+
 // function Manager(name, employeeID, email, officeNumber) {
     
 //     if (typeof name !== "string" || !name.trim().length) {
